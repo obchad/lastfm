ee298f6b782d026d0554f4dfbd933208e791c79e
Chad <ob.chad@gmail.com>
2017-07-17 20:56:24 +1000

    cleanup

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -1,29 +1,35 @@
 var lastfmApp = angular.module('lastfmApp', []);
 
-lastfmApp.controller('lastfmController', function ($scope, $http) {
-    $http.get('http://localhost:8080/rest/lastfm/findall')
-        .then(function (response) {
-            $scope.result = response.data;
-        });
+lastfmApp.controller('fetchArtistController', function($scope, $http) {
 
-});
-
-lastfmApp.controller('fetchArtistController', function ($scope, $http) {
-    $scope.searchArtist = function () {
-        $http({
-            method: 'GET',
-            params: {country: $scope.country},
-            url: 'http://localhost:8080/rest/lastfm/artists'
-        }).then(function (response) {
+    $scope.main = {
+        page: 1
+    };
+    $scope.searchArtist = function() {
+        $http({method: 'GET',
+            params: {start : $scope.startPosition},
+            url: 'http://localhost:8080//LastFmRedBallon/rest/lastfm/artists/'+$scope.country+'?start='+$scope.main.page}).
+        then(function(response) {
             if (response.data == null) {
                 $scope.message = "No artist found";
             } else {
                 $scope.results = response.data;
                 $scope.message = "";
             }
-
         });
     }
+
+    $scope.nextPage = function() {
+        $scope.main.page++;
+        $scope.searchArtist();
+    };
+
+    $scope.previousPage = function() {
+        if ($scope.main.page > 1) {
+            $scope.main.page--;
+            $scope.searchArtist();
+        }
+    };
 });
 			
 		 
